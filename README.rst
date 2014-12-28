5c3d5046e7017fde08ce27653e21483b04ce597f
Tres Seaver <tseaver@palladion.com>
2014-12-28 00:58:16 -0500

    Initial project skeleton.

---
+++ README.rst
@@ -0,0 +1,11 @@
+``travis_notify``
+=================
+
+Forward Travis-CI webhook notifications to the ``zope-tests@zope.org``
+mailing list.
+
+See:
+
+- http://docs.travis-ci.com/user/notifications/#Webhook-notification
+
+- http://docs.zope.org/zopetoolkit/process/buildbots.html#informing-the-zope-developer-community-about-build-results
---
+++ setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.rst')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_sendmail',
+    'pyramid_tm',
+    'pyramid_zodbconn',
+    'transaction',
+    'ZODB3',
+    'waitress',
+    ]
+
+setup(name='travis_notify',
+      version='0.1.dev0',
+      description='travis_notify',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="travis_notify",
+      entry_points="""\
+      [paste.app_factory]
+      main = travis_notify:main
+      """,
+      )
